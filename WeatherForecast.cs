b270e1a5ff87682cf0903a98e2f0be4583f8a2a9
crystalchavez99 <chavezcrystal1999@yahoo.com>
2024-01-02 12:02:51 -0800

    first commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace build_web_api_aspnet_core;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
